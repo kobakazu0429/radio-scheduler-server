c2d21f176c7efdcc8403ba4a4adf05545ca489df
kobakakzu0429 <kobakazu10@gmail.com>
2018-07-01 00:30:15 +0900

    fix import.py

--- import.py
+++ import.py
@@ -5,25 +5,25 @@ from datamodel import *
 
 
 # tsvファイルを一行ずつ読み込んでタブで分割し，それぞれをデータベースに登録
-for line in open("data.tsv", "r"):
+for line in open("data.csv", "r"):
     (time, title, manager, published_at, recorded, edited, reviewed, drew_thumbnail, reserved, released, drew_comic,
-     tweeted, folder_id, record_url, thumbnail_url, comic_url) = tuple(line[:-1].split("\t"))
+     tweeted, folder_id, record_url, thumbnail_url, comic_url) = tuple(line[:-1].split(","))
 
     Tasks.create(
-        time=time
-        title=title
-        manager=manager
-        published_at=published_at
-        recorded=recorded
-        edited=edited
-        reviewed=reviewed
-        drew_thumbnail=drew_thumbnail
-        reserved=reserved
-        released=released
-        drew_comic=drew_comic
-        tweeted=tweeted
-        folder_id=folder_id
-        record_url=record_url
-        thumbnail_url=thumbnail_url
+        time=time,
+        title=title,
+        manager=manager,
+        published_at=published_at,
+        recorded=int(recorded),
+        edited=int(edited),
+        reviewed=int(reviewed),
+        drew_thumbnail=int(drew_thumbnail),
+        reserved=int(reserved),
+        released=int(released),
+        drew_comic=int(drew_comic),
+        tweeted=int(tweeted),
+        folder_id=folder_id,
+        record_url=record_url,
+        thumbnail_url=thumbnail_url,
         comic_url=comic_url
-    }
+    )
